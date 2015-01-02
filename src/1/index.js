0cbf946ea33827bc4516884fa768b004993fff81
upinetree <t.matsumoto@everyleaf.com>
2015-01-02 17:50:57 +0900

    Check JSX rule

--- src/1/index.js
+++ src/1/index.js
@@ -1,9 +1,30 @@
 var Hello = React.createClass({
   render: function() {
     return (
-      <div>Hello {this.props.name}</div>
+      <div>Hello {this.props.name}!!</div>
     );
   }
 })
 
-React.render(<Hello name="World" />, document.body);
+// var MustBeWrapped= React.createClass({
+//   render: function() {
+//     return (
+//       <div>XJS elements</div>
+//       <div>must be wrapped</div>
+//     );
+//   }
+// })
+
+var MustBeWrapped= React.createClass({
+  render: function() {
+    return (
+      <div>
+        <div>XJS elements</div>
+        <div>must be wrapped</div>
+      </div>
+    );
+  }
+})
+
+React.render(<Hello name="React" />, document.getElementById("hello"));
+React.render(<MustBeWrapped />, document.getElementById("app"));
