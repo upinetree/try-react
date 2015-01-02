20945293e014daf90014f4a126d64f2976ba9583
upinetree <t.matsumoto@everyleaf.com>
2015-01-02 18:44:01 +0900

    Add practices of composing components

---
+++ src/2/index.js
@@ -0,0 +1,45 @@
+var Comment = React.createClass({
+  render: function() {
+    return (
+      <div className="comment">
+        <h3 className="commentAuthor">{this.props.author}</h3>
+        {this.props.children}
+      </div>
+    );
+  }
+})
+
+var CommentList = React.createClass({
+  render: function() {
+    return (
+      <div className="commentList">
+        <Comment author="Foo">Hellooooo</Comment>
+        <Comment author="Bar">Wooooorld</Comment>
+      </div>
+    );
+  }
+})
+
+var CommentForm = React.createClass({
+  render: function() {
+    return (
+      <div className="commentForm">
+        I am a CommentForm
+      </div>
+    );
+  }
+})
+
+var CommentBox = React.createClass({
+  render: function() {
+    return (
+      <div className="commentBox">
+        <h2>Comments</h2>
+        <CommentList />
+        <CommentForm />
+      </div>
+    );
+  }
+})
+
+React.render(<CommentBox />, document.getElementById("comments"));
